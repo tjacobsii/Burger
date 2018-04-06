09746999964854be4c07d6f3f1ab463e52e7eb22
Thomas Jacobs II <thomasjacobsii@yahoo.com>
2018-04-05 22:15:38 -0400

    Updates

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+SELECT * FROM burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Sandwich', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseburger', FALSE);
